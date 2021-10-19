be9839864f2eecf40aa9dac7a2ea8d80cb0d78e6
shaan <shaanmohd372@gmail.com>
2021-10-19 18:04:04 +0530

    commting the file

---
+++ Article.js
@@ -0,0 +1,10 @@
+artimage={
+    0:"https://images.unsplash.com/photo-1594950195709-a14f66c242d7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjB8fDcyMHxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    1: "https://images.unsplash.com/photo-1605557713876-148ede2bbb3c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fDcyMHxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    2:"https://images.unsplash.com/photo-1552962730-61f41ae62092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njh8fDcyMHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60",
+    3:"https://images.unsplash.com/photo-1552962646-cce5d3785e10?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fDcyMHxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    4:"https://images.unsplash.com/photo-1620064856565-b5efac6e14ca?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjZ8fDcyMHxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    5:"https://images.unsplash.com/photo-1542362567-b07e54358753?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njl8fDcyMHxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    6:"https://images.unsplash.com/photo-1615394767355-b2a71c95468c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTl8fDcyMHxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    7:"https://images.unsplash.com/photo-1592424406134-434746740477?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"
+}
