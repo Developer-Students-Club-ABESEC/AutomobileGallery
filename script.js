c99c35db65238598075266402bf3137c4b28da47
Dayanand M.G <dayanandadaya8055@gmail.com>
2021-10-24 22:03:14 +0530

    My new commit

---
+++ script.js
@@ -0,0 +1,53 @@
+let spinnerwrapper = document.querySelector('.spinner-wrapper');
+
+window.addEventListener ('load', function () {
+    spinnerwrapper.parentElement.removeChild(spinnerwrapper);
+});
+
+// Adding a popup script file to js
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+
+// Adding javascript API into js file but specify the browser wanted
+
+function logTabs(tabs) {
+    console.log(tabs)
+  }
+  
+  browser.tabs.query({currentWindow: true}, logTabs)
